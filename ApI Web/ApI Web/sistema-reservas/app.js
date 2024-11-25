fe99cc71463049336c17161754e4c33b61c2bdd1
Ronny Joel Ibarra Gaona <96481164+RonnyIbarra@users.noreply.github.com>
2024-11-25 10:39:03 -0500

    API

---
+++ ApI Web/ApI Web/sistema-reservas/app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const reservasRouter = require('./routes/reservas.routes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.json());
+const cors = require('cors');
+app.use(cors());
+// Rutas
+app.use('/api/reservas', reservasRouter);
+
+// Iniciar servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
